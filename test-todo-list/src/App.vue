05829b6ce9f78b24b50010fe5fc6e5ccbce65604
vasiliy <avtoroku66@gmail.com>
2023-08-17 16:47:51 +0300

    feat: add basic functionality(input form, add task, editing task, save/cancel)

--- test-todo-list/src/App.vue
+++ test-todo-list/src/App.vue
@@ -1,21 +1,108 @@
 <template>
-  <div id="app">
+  <div>
+    <AddTask @task-added="addTask" />
     
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <template v-if="task.editing">
+          <input v-model="task.text" @keyup.enter="saveTask(task)" @keyup.esc="cancelEdit(task)" />
+          <button @click="saveTask(task)" class="save-button">Сохранить</button>
+          <button @click="cancelEdit(task)" class="cancel-button">Отмена</button>
+        </template>
+        <template v-else>
+          {{ task.text }}
+          <button @click="editTask(task)">Редактировать</button>
+        </template>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import AddTask from '@/components/AddTask.vue';
 
 export default {
+  components: {
+    AddTask
+  },
   data() {
     return {
-      title: "Привет, Vue!",
-      message: "Это мой первый компонент в Vue."
+      tasks: []
     };
+  },
+  methods: {
+    addTask(newTaskText) {
+      this.tasks.push({ id: Date.now(), text: newTaskText, editing: false });
+    },
+    editTask(task) {
+      task.editing = true;
+    },
+    saveTask(task) {
+      task.editing = false;
+    },
+    cancelEdit(task) {
+      task.editing = false;
+    }
   }
-}
+};
 </script>
 
-<style>
 
+
+<style>
+  body {
+    margin: 0;
+    padding: 60px;
+    margin: 0 auto;
+    font-family: WorkSans-Medium;
+    max-width: 1440px;
+    background-color: gainsboro;
+    /* background-attachment: fixed; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  p {
+    font-size: 24px;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style-type: none;
+    padding: 4px;
+    margin: 4px;
+    border: 2px solid black;
+  }
+
+  button {
+    border-radius: 4px;
+  }
+
+
+  .task-list {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    width: 1400px;
+    height: 700px;
+    display: flex;
+    justify-content: center;
+    background-color: blanchedalmond;
+  }
+
+  @font-face {
+    font-family: WorkSans-Medium;
+    src: url(/fonts/WorkSans-Medium.ttf);
+  }
 </style>
+
+
+
+
--- test-todo-list/src/components/AddTask.vue
+++ test-todo-list/src/components/AddTask.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="add-task">
+  <div class="addTask">
+    <input v-model="newTaskText" placeholder="Enter task" class="input" maxlength="100" />
+    <button @click="addTask">Add</button>
   </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      newTaskText: ''
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTaskText.trim() !== '') {
+        this.$emit('task-added', this.newTaskText);
+        this.newTaskText = '';
+      }
+    }
+  }
+};
 </script>
 
 <style>
+  .addTask {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
+  .input {
+    margin: 15px;
+    width: 300px;
+    height: 30px;
+    font-size: 20px;
+  }
 </style>
